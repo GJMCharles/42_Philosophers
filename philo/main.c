41767d9518874b810964e1245a1422db8d20825e
GJM Charles <91600327+GJMCharles@users.noreply.github.com>
2025-11-25 23:55:48 +0100

    update

--- philo/main.c
+++ philo/main.c
@@ -12,12 +12,6 @@
 
 #include "philo.h"
 
-// number_of_philosophers
-// time_to_die
-// time_to_eat
-// time_to_sleep
-// number_of_times_each_philosopher_must_eat
-
 int	verify_arguments(int nb_args, char *args[])
 {
 	(void) *args;
