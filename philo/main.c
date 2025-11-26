3f9e3b16322b4268411f26ccb8a2ffecf2a49dd4
GJM Charles <91600327+GJMCharles@users.noreply.github.com>
2025-11-26 23:56:19 +0100

    update main.c

--- philo/main.c
+++ philo/main.c
@@ -12,6 +12,7 @@
 
 #include "philo.h"
 
+/**
 int	verify_data(int nb_args, char *args[])
 {
 	(void) *args;
@@ -38,24 +39,37 @@ int	assign_data_list(t_data *philo, char *argv[])
 	(void) *argv;
 	return (0);
 }
+ */
+
+int	verify_data(int argc, char *argv[])
+{
+	int	index;
+
+	index = 0;
+	while (++index < argc)
+	{
+		printf(argv[index]);
+	}
+	return (0);
+}
 
 int	main(int argc, char *argv[])
 {
-	t_data	philo;
+	// t_data	philo;
 
 	if (argc < 5 || argc > 6)
 		return (1);
 	if (!verify_data(argc, argv))
 		return (1);
-	if (assign_data_list(&philo, argv))
-		return (1);
-	philo.nb_philos = argv[1];
-	philo.t_die = argv[2];
-	philo.t_eat = argv[3];
-	philo.t_sleep = argv[4];
-	if (argc == 6)
-		philo.nb_t_eat = argv[5];
-	initiate_thread(&philo);
-	exit_thread(&philo);
+	// if (assign_data_list(&philo, argv))
+	// 	return (1);
+	// philo.nb_philos = argv[1];
+	// philo.t_die = argv[2];
+	// philo.t_eat = argv[3];
+	// philo.t_sleep = argv[4];
+	// if (argc == 6)
+	// 	philo.nb_t_eat = argv[5];
+	// initiate_thread(&philo);
+	// exit_thread(&philo);
 	return (0);
 }
