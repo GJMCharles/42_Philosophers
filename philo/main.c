2b169d239a4f85fd610d5fdd6919d529e12e87c8
GJM Charles <91600327+GJMCharles@users.noreply.github.com>
2025-11-27 10:08:11 +0100

    update

--- philo/main.c
+++ philo/main.c
@@ -32,44 +32,48 @@ void	exit_thread(t_data *data)
 {
 	//
 }
+*/
 
-int	assign_data_list(t_data *philo, char *argv[])
+void	assign_data_list(t_data **philo, char **argv)
 {
-	(void) philo;
-	(void) *argv;
-	return (0);
-}
- */
+	t_data	*temp;
 
-int	verify_data(int argc, char *argv[])
+	temp = *philo;
+	temp->nb_philos = ft_atoi(argv[1]);
+	temp->t_die = ft_atoi(argv[2]);
+	temp->t_eat = ft_atoi(argv[3]);
+	temp->t_sleep = ft_atoi(argv[4]);
+	if (argv[5] != NULL)
+		temp->nb_t_eat = ft_atoi(argv[5]);
+}
+
+int	verify_data(int argc, char **argv)
 {
 	int	index;
+	int	value;
 
 	index = 0;
 	while (++index < argc)
 	{
-		printf(argv[index]);
+		value = ft_atoi(argv[index]);
+		if (!value || value < 0)
+			return (0);
 	}
-	return (0);
+	return (1);
 }
 
 int	main(int argc, char *argv[])
 {
-	// t_data	philo;
+	t_data	*philo;
 
 	if (argc < 5 || argc > 6)
-		return (1);
+		return (EXIT_FAILURE);
 	if (!verify_data(argc, argv))
-		return (1);
-	// if (assign_data_list(&philo, argv))
-	// 	return (1);
-	// philo.nb_philos = argv[1];
-	// philo.t_die = argv[2];
-	// philo.t_eat = argv[3];
-	// philo.t_sleep = argv[4];
-	// if (argc == 6)
-	// 	philo.nb_t_eat = argv[5];
-	// initiate_thread(&philo);
-	// exit_thread(&philo);
-	return (0);
+		return (EXIT_FAILURE);
+	philo = (t_data *) malloc(sizeof(t_data));
+	if (!philo)
+		return (EXIT_FAILURE);
+	assign_data_list(&philo, argv);
+	free(philo);
+	return (EXIT_SUCCESS);
 }
